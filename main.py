73ab7c8bba31a1a8490b1c45ba1926edcdc096c9
Glenn Pinto <gvpinto@gmail.com>
2025-09-27 20:32:57 -0400

    Checkpoint memory with SQLite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Type, TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 class State(TypedDict):
     input: str
@@ -29,7 +31,11 @@ builder.add_edge("step_1", "human_feedback")
 builder.add_edge("human_feedback", "step_3")
 builder.add_edge("step_3", END)
 
-memory = MemorySaver()
+
+conn = sqlite3.connect("checkpoints.sqlite", check_same_thread=False)
+# memory = MemorySaver()
+memory = SqliteSaver(conn)
+# memory = SqliteSaver.from_conn_string("checkpoints.sqlite")
 
 graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"])
 
@@ -37,16 +43,16 @@ graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"]
 graph.get_graph().draw_mermaid_png(output_file_path="state_graph.png")
 
 if __name__ == "__main__":
-    thread = {"configurable": {"thread_id": "1"}}
+    thread = {"configurable": {"thread_id": "777"}}
 
-    initial_input = {"input": "Hello world!"}
+    # initial_input = {"input": "Hello world!"}
 
-    for event in graph.stream(initial_input, thread, stream_mode="values"):
-        print(event)
-    # for event in graph.stream(None, thread, stream_mode="values"):
+    # for event in graph.stream(initial_input, thread, stream_mode="values"):
     #     print(event)
+    # # for event in graph.stream(None, thread, stream_mode="values"):
+    # #     print(event)
 
-    print(graph.get_state(thread).next)
+    # print(graph.get_state(thread).next)
 
     user_input = input("Tell me how you want to update the state:")
 
